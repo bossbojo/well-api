5ac46ae419c8158894e618498205cb0e65772a2b
paramat <paramat.si.56@ubu.ac.th>
2018-12-07 16:28:36 +0700

    first commit

--- WebApi/Controllers/ValuesController.cs
+++ WebApi/Controllers/ValuesController.cs
@@ -4,36 +4,17 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using ThaiNationalIDCard;
 
 namespace WebApi.Controllers
 {
     public class ValuesController : ApiController
     {
-        // GET api/values
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        public void Delete(int id)
-        {
+        [Route("api/read/card")]
+        public IHttpActionResult getReaderCard() {
+            ThaiIDCard idcard = new ThaiIDCard();
+            Personal personal = idcard.readAllPhoto();
+            return Json(personal);
         }
     }
 }
